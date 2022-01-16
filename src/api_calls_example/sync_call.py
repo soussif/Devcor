36d5842a28d191f36e255b8589124abd45638500
Faek Soussi <soussif@gmail.com>
2022-01-16 11:10:04 -0800

    add example of asyncio api call to restconf

---
+++ src/api_calls_example/sync_call.py
@@ -0,0 +1,16 @@
+import requests
+
+
+base_url = 'http://httpbin.org/'
+
+def get_delay(seconds):
+    endpoint = f'/delay/{seconds}'
+    
+    print(f'Getting with {seconds} delay...')
+    
+    resp = requests.get(base_url+endpoint)
+    data = resp.json()
+    print(data)
+
+get_delay(5)
+print('Okay! All finished getting.')
