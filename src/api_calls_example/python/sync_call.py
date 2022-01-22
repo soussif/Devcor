fa8f7c3574533250e943882f1a945c006b507da8
Faek Soussi <faek.soussi@imc.com>
2022-01-22 15:33:02 +0100

    fixed folder struc and added few more exmaples

--- src/api_calls_example/python/sync_call.py
+++ src/api_calls_example/python/sync_call.py
@@ -2,15 +2,18 @@ import requests
 
 
 base_url = 'http://httpbin.org/'
+endpoing = "anything"
+
 
 def get_delay(seconds):
     endpoint = f'/delay/{seconds}'
-    
+
     print(f'Getting with {seconds} delay...')
-    
-    resp = requests.get(base_url+endpoint)
+
+    resp = requests.get(base_url + endpoint)
     data = resp.json()
     print(data)
+
 
 get_delay(5)
 print('Okay! All finished getting.')
